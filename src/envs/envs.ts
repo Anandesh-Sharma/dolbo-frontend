87a132ec2b0512d315c6f2bf7e32639306c27a84
Pratyush Sharma <pratyush.sharma618@gmail.com>
2025-03-22 00:03:38 +0530

    Update env.ts

--- src/envs/envs.ts
+++ src/envs/envs.ts
@@ -1,5 +1,5 @@
 export const API_HOST =
-  'https://93d1-2405-201-5800-2854-4c6c-d1a5-20ff-d2a8.ngrok-free.app';
+  'http://localhost:8000';
 
 export const API_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImVkNjViMmQ0LWQyNGYtNGVjZC1iYzM2LTQxYTBiMjZkNzUzMCIsImV4cCI6MTczODEwMDYxNn0.92P3rkfpVL3hn4Ftnbau0R2VfPkwY1RnCRKstf5l7aI';
 
