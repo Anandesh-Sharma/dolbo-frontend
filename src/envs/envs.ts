c39b95bbea43b768a16a213a058c65ae4bba9991
Anandesh Sharma <anandeshsharma@gmail.com>
2025-03-23 20:09:34 +0000

    docker and env fixes for deployment

--- src/envs/envs.ts
+++ src/envs/envs.ts
@@ -1,4 +1,4 @@
-export const API_HOST = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+export const API_HOST = 'https://api-beta.dolbo.ai';
 
 // Remove the hardcoded token since we're using dynamic auth now
 // export const API_TOKEN = '...';
